04518445e636d6a0611560c96a6f935d70688034
E Alexander <erick.interiano05@gmail.com>
2021-04-08 19:04:59 -0700

    made get route for range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,4 +18,14 @@ module.exports = function(app){
             res.json(err)
         })
     });
+
+    app.get("/api/workouts/range", function(req, res) {
+        Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
 }
