ffd36d1be80b23c978bbcc6e69310eb96cf1d1e8
E Alexander <erick.interiano05@gmail.com>
2021-04-08 17:28:25 -0700

    made get route for workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,13 @@
-const Workout = require("../models/workout")
+const Workout = require("../models/workout.js");
+
+module.exports = function(app){
+    app.get("/api/workouts", function(req , res){
+        Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    })
+}
